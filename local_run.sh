c18b177b621b872708e62818b421391caca0f869
Nathan Dister <nathan.dister@adhoc.team>
2022-01-18 13:03:07 -0500

    Add dockerfile, local build and run scripts

---
+++ local_run.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+get_parameter() {
+    aws --output text --query Parameter.Value ssm get-parameter --name $1 --with-decryption 
+}
+
+export SERVER_PORT="$(get_parameter '/bb2/test/python-simple-https-server/server-port')"
+export SERVER_KEY="$(get_parameter '/bb2/test/python-simple-https-server/server-key')"
+export SERVER_CERT="$(get_parameter '/bb2/test/python-simple-https-server/server-cert')"
+
+#python3 main.py
+docker run --rm \
+    -e SERVER_PORT \
+    -e SERVER_KEY \
+    -e SERVER_CERT \
+    -p $SERVER_PORT:$SERVER_PORT \
+    python-simple-https-server:latest
