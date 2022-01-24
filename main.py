ed63dc9e05080c61556be4898c056e3cd4477b43
Nathan Dister <nathan.dister@adhoc.team>
2022-01-24 14:56:52 -0500

    Refactor SimpleHTTPSRequestHandler class to composably accept parameters: https://stackoverflow.com/a/52046062

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ import os
 import ssl
 import sys
 
-server_cpu_endpoint_iterations = 0
+from functools import partial
 
 class SimpleHTTPSRequestHandler(http.server.BaseHTTPRequestHandler):
+
+    def __init__(self, server_cpu_endpoint_iterations, *args, **kwargs):
+        self.server_cpu_endpoint_iterations = server_cpu_endpoint_iterations
+
+        super().__init__(*args, **kwargs)
 
     def do_GET(self):
         if self.path == "/":
@@ -17,7 +22,7 @@ class SimpleHTTPSRequestHandler(http.server.BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(b"{\"message\":\"server healthy\"}")
         elif self.path == "/cpu":
-            expensive_value = str(sum([i for i in range(server_cpu_endpoint_iterations)]))
+            expensive_value = str(sum([i for i in range(self.server_cpu_endpoint_iterations)]))
             self.send_response(200)
             self.end_headers()
             self.wfile.write(b"{\"message\":\"%b\"}" % bytes(expensive_value, "utf-8"))
@@ -40,13 +45,15 @@ def main():
             server_cert_file.write(os.environ["SERVER_CERT"])
             server_cert_file.close()
 
-        global server_cpu_endpoint_iterations
         server_cpu_endpoint_iterations = int(os.environ["SERVER_CPU_ENDPOINT_ITERATIONS"])
 
     except Exception as e:
         sys.exit(e)
 
-    server = http.server.HTTPServer(("0.0.0.0", server_port), SimpleHTTPSRequestHandler)
+    # Solved composabililty issue with BaseHTTPRequestHandler via this solution: https://stackoverflow.com/a/52046062
+    req_handler = partial(SimpleHTTPSRequestHandler, server_cpu_endpoint_iterations)
+
+    server = http.server.HTTPServer(("0.0.0.0", server_port), req_handler)
 
     server.socket = ssl.wrap_socket(server.socket,
         keyfile="/tmp/key.pem",
--- main_test.py
+++ main_test.py
@@ -4,17 +4,19 @@ import unittest
 import urllib.request
 import urllib.error
 
+from functools import partial
+
 from main import SimpleHTTPSRequestHandler
 
 TESTING_IP="127.0.0.1"
 TESTING_PORT=8080
-TESTING_URL=f"http://{TESTING_IP}:{TESTING_PORT}"
 
 class TestSimpleHTTPSRequestHandler(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls._server = http.server.HTTPServer((TESTING_IP, TESTING_PORT), SimpleHTTPSRequestHandler)
+        cls._req_handler = partial(SimpleHTTPSRequestHandler, 10000000)
+        cls._server = http.server.HTTPServer((TESTING_IP, TESTING_PORT), cls._req_handler)
         cls._server_thread = threading.Thread(target=cls._server.serve_forever)
         cls._server_thread.start()
 
@@ -23,31 +25,30 @@ class TestSimpleHTTPSRequestHandler(unittest.TestCase):
         cls._server.shutdown()
 
     def test_root(self):
-        res = urllib.request.urlopen(f"{TESTING_URL}/")
+        res = urllib.request.urlopen(f"http://{TESTING_IP}:{TESTING_PORT}/")
         res_body = res.read().decode("utf-8")
 
         self.assertEqual(res.status, 200)
         self.assertEqual(res_body, "{\"message\":\"hello world!\"}")
 
     def test_health(self):
-        res = urllib.request.urlopen(f"{TESTING_URL}/health")
+        res = urllib.request.urlopen(f"http://{TESTING_IP}:{TESTING_PORT}/health")
         res_body = res.read().decode("utf-8")
 
         self.assertEqual(res.status, 200)
         self.assertEqual(res_body, "{\"message\":\"server healthy\"}")
 
     def test_cpu(self):
-        res = urllib.request.urlopen(f"{TESTING_URL}/cpu")
+        res = urllib.request.urlopen(f"http://{TESTING_IP}:{TESTING_PORT}/cpu")
         res_body = res.read().decode("utf-8")
 
         self.assertEqual(res.status, 200)
-        # TODO refactor parameterization of SimpleHTTPSRequestHandler so this can be tested better
-        self.assertEqual(res_body, "{\"message\":\"0\"}")
+        self.assertEqual(res_body, "{\"message\":\"49999995000000\"}")
 
     def test_404(self):
         # Must wrap tests here in assertRaises to handle HTTPError from urllib
         with self.assertRaises(urllib.error.HTTPError):
-            res = urllib.request.urlopen(f"{TESTING_URL}/fake")
+            res = urllib.request.urlopen(f"http://{TESTING_IP}:{TESTING_PORT}/fake")
             res_body = res.read().decode("utf-8")
 
             self.assertEqual(res.status, 404)
